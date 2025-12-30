9b760b61bb214a7a58f041cb0466e3813205a566
PresiTK <sergi.gonzalez.cabrera@enti.cat>
2025-12-30 18:57:57 +0100

    push things

--- app/src/main/java/com/example/myapplication/screens/CompsListScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/CompsListScreen.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication.screens
 
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -15,16 +14,12 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.myapplication.Screen
-
-private val BlendMode.CompositionDetail: Any
 
 data class Composition(
     val name: String,
--- app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
@@ -10,37 +10,98 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFF276A49),
+    onPrimary = Color(0xFFFFFFFF),
+    primaryContainer = Color(0xFFADF2C7),
+    onPrimaryContainer = Color(0xFF055232),
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondary = Color(0xFF4E6355),
+    onSecondary = Color(0xFFFFFFFF),
+    secondaryContainer = Color(0xFFD0E8D6),
+    onSecondaryContainer = Color(0xFF374B3E),
+
+    tertiary = Color(0xFF3C6471),
+    onTertiary = Color(0xFFFFFFFF),
+    tertiaryContainer = Color(0xFFBFE9F9),
+    onTertiaryContainer = Color(0xFF224C59),
+
+    error = Color(0xFFBA1A1A),
+    onError = Color(0xFFFFFFFF),
+    errorContainer = Color(0xFFFFDAD6),
+    onErrorContainer = Color(0xFF93000A),
+
+    background = Color(0xFFF6FBF4),
+    onBackground = Color(0xFF171D19),
+
+    surface = Color(0xFFF6FBF4),
+    onSurface = Color(0xFF171D19),
+    surfaceVariant = Color(0xFFDCE5DC),
+    onSurfaceVariant = Color(0xFF404942),
+
+    outline = Color(0xFF717972),
+    outlineVariant = Color(0xFFC0C9C0),
+
+    inverseSurface = Color(0xFF2C322D),
+    inverseOnSurface = Color(0xFFEDF2EB),
+    inversePrimary = Color(0xFF91D5AC),
+
+    surfaceTint = Color(0xFF276A49),
+    scrim = Color(0xFF000000)
 )
+
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Color(0xFF91D5AC),
+    onPrimary = Color(0xFF003921),
+    primaryContainer = Color(0xFF055232),
+    onPrimaryContainer = Color(0xFFADF2C7),
+
+    secondary = Color(0xFFB5CCBB),
+    onSecondary = Color(0xFF213528),
+    secondaryContainer = Color(0xFF374B3E),
+    onSecondaryContainer = Color(0xFFD0E8D6),
+
+    tertiary = Color(0xFFA4CDDC),
+    onTertiary = Color(0xFF043541),
+    tertiaryContainer = Color(0xFF224C59),
+    onTertiaryContainer = Color(0xFFBFE9F9),
+
+    error = Color(0xFFFFB4AB),
+    onError = Color(0xFF690005),
+    errorContainer = Color(0xFF93000A),
+    onErrorContainer = Color(0xFFFFDAD6),
+
+    background = Color(0xFF0F1511),
+    onBackground = Color(0xFFDFE4DD),
+
+    surface = Color(0xFF0F1511),
+    onSurface = Color(0xFFDFE4DD),
+    surfaceVariant = Color(0xFF404942),
+    onSurfaceVariant = Color(0xFFC0C9C0),
+
+    outline = Color(0xFF8A938B),
+    outlineVariant = Color(0xFF404942),
+
+    inverseSurface = Color(0xFFDFE4DD),
+    inverseOnSurface = Color(0xFF2C322D),
+    inversePrimary = Color(0xFF276A49),
+
+    surfaceTint = Color(0xFF91D5AC),
+    scrim = Color(0xFF000000)
+)
+
 
 @Composable
 fun MyApplicationTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -49,16 +110,17 @@ fun MyApplicationTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
     val view = LocalView.current
+
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
